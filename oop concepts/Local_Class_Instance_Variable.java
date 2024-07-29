c63988daad1184e9bd1db26cc7fab4886ec58c19
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2024-07-30 00:59:54 +0600

    local class and instance variable added

---
+++ oop concepts/Local_Class_Instance_Variable.java
@@ -0,0 +1,10 @@
+public class Local_Class_Instance_Variable {
+    String name; // ! Instance variable
+    int id; // $ Instance variable
+    static String University_Name = "Gono University"; // ? class or static variable
+
+    Local_Class_Instance_Variable(String name, int id) { // # Local variable
+        this.name = name;
+        this.id = id;
+    }
+}
