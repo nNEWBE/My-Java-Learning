decb4b3bf99c950f886cbe9b7a6479dbdf506038
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2024-08-02 00:12:43 +0600

    difference between abstract class and interface added

---
+++ oop concepts/Interface_vs_Abstract_Class.java
@@ -0,0 +1,16 @@
+public class Interface_vs_Abstract_Class {
+
+    // ! Similarity
+    // * (i) Interface can have any number of methods
+    // ? (ii) It has the same file extension as class (.java)
+
+    // ! Difference
+    // * (i) You can not initialize a interface
+    // ? (ii) Interface doesn't contain any constructor
+    // do (iii) All methods in interface are abstract
+    // @ (iv) An interface can't have instance variables
+    // $ (v) An interface can extend multiple inheritance
+    //// (vi) Abstract class is parallely abstract and interface is fully abstract
+    // # (vii) Interface have only static and final variables but Abstract class has static, non-static, final, non-final variables
+
+}
